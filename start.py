621a97370e682b047aa34d1a442f37c5df3d003e
Joe Raccio <joseph.a.raccio@Uconn.edu>
2021-08-25 11:13:27 -0400

    Moved Join/leave to seperate function

--- start.py
+++ start.py
@@ -18,25 +18,28 @@ async def on_message(message):
     # I want to find a more elegent solution to functions
     # Currently I am using this method just to understand how it works
     if message.content.startswith('$join'):
-        for channel in message.guild.voice_channels:
-            if message.author.id in [x.id for x in channel.members]:
-                current_vc = message.guild.voice_client
-                if current_vc:
-                    await current_vc.disconnect()
-                await channel.connect()
-                break
+        await join_channel(message)
 
     elif message.content.startswith('$leave'):
-        current_vc = message.guild.voice_client
-        await current_vc.disconnect()
+        await leave_channel(message)
+        
     elif message.content.startswith('$'):
         #await message.channel.send('Hello!')
         
         #This is for testing the internals
         print(message.content[1:])
         exec(message.content[1:])
-    
 
+async def join_channel(message):
+    for channel in message.guild.voice_channels:
+        if message.author.id in [x.id for x in channel.members]:
+            await leave_channel(message)
+            await channel.connect()
+            break
+
+async def leave_channel(message):
+    if message.guild.voice_client:
+        await message.guild.voice_client.disconnect()
 
 
 f = open("token.txt", 'r')
