8ddbbe00f7d41d1665028046fc48355659ee2ad0
Joe Raccio <joseph.a.raccio@Uconn.edu>
2021-10-02 11:46:04 -0400

    Case insensitive commands

--- main.py
+++ main.py
@@ -8,16 +8,18 @@ from music import Music
 #Discord Library Needed
 
 
-bot = commands.Bot(command_prefix=commands.when_mentioned_or("$"))
+bot = commands.Bot(command_prefix='$', case_insensitive=True)
 bot.add_cog(Music(bot))
 
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user} (ID: {bot.user.id})')
     print('------')
+    
 
 f = open("token.txt", 'r')
 token = f.readline().strip()
 f.close()
+beat_bot_id = 880083788955279410
 
 bot.run(token)
