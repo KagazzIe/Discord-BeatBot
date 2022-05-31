4575a7cfd004178682446bb2cd0d1551d9aafda8
Joe Raccio <joseph.a.raccio@uconn.edu>
2022-05-31 04:59:25 -0400

    config setup

---
+++ beatbot-code/src/main.js
@@ -0,0 +1,18 @@
+const { Client, Intents } = require("discord.js");
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
+});
+const config = require("../config.json");
+
+client.on("ready", () => {
+  console.log("I am ready!");
+});
+
+client.on("messageCreate", (message) => {
+  if (message.content.startsWith("ping")) {
+    message.channel.send("pong!");
+  }
+});
+
+
+client.login(config.token);
